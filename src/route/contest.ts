1428bdd86032a89da2f6d5322d04829dfb3dc6fd
TarakNathJana <janatarak739@gmail.com>
2026-01-13 20:00:51 +0530

    test post2

--- src/route/contest.ts
+++ src/route/contest.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
-import {createContest ,getContestData ,createMCQ  } from "../contoller/index.js";
-import { authenticate, isCreator } from "../middleware/index.js";
+import {
+  createContest,
+  getContestData,
+  createMCQ,
+  submitMCQAnswers,
+} from "../contoller/index.js";
+import { authenticate, isCreator, isContestee } from "../middleware/index.js";
 
 const router = Router();
 
 router.post("/contests", authenticate, isCreator, createContest);
-router.get("/contests/:contestId",authenticate ,getContestData);
+router.get("/contests/:contestId", authenticate, getContestData);
 router.post("/contests/:contestId/mcq", authenticate, isCreator, createMCQ);
+
+router.post(
+  "/api/contests/:contestId/mcq/:questionId/submit",
+  authenticate,
+  isContestee,
+  submitMCQAnswers
+);
 export default router;
